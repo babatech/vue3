8a60432e7ffac5eba00b1bea69b7fd166757b71d
Shahab Shakoor <shahabi1209@gmail.com>
2020-09-28 22:37:20 +0200

    code refector

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,13 +3,18 @@
     <div class="row">
       <div class="col-4 Options">
         <div class="categories">
-        <b>Filter By:</b>
-        <a v-on:click="filterBy(category)"  v-for="category in categories" v-bind:key="category">{{category}}</a>
+          <b>Filter By:</b>
+          <a
+            v-on:click="filterBy(category)"
+            v-for="category in categories"
+            v-bind:key="category"
+            >{{ category }}</a
+          >
         </div>
         <div class="further-options">
-        <b>Subscribe:</b>
-        <a v-on:click="subscribeRSS()">RSS</a>
-        <a v-on:click="subscribeEmail()">Email</a>
+          <b>Subscribe:</b>
+          <a v-on:click="subscribeRSS()">RSS</a>
+          <a v-on:click="subscribeEmail()">Email</a>
         </div>
       </div>
       <div class="col-8">
@@ -23,43 +28,40 @@
 </template>
 
 <script>
-import Message from '@/components/Message.vue'
+import Message from "@/components/Message.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    Message
+    Message,
   },
-  data(){
+  data() {
     return {
-      messages: [1,2,3],
-      categories: [1,2]
-    }
+      messages: [1, 2, 3],
+      categories: [1, 2],
+    };
   },
-  mounted () {},
+  mounted() {},
   methods: {
     filterBy(category) {
-      console.log(category)
+      console.log(category);
     },
-    subscribeRSS() {
-
-    },
-    subscribeEmail() {
-      
-    }
-  }
-}
+    subscribeRSS() {},
+    subscribeEmail() {},
+  },
+};
 </script>
 
 <style lang="scss">
-.Options{
+.Options {
   text-align: left;
- a{
-   display: block;
- }
- .categories,
- .further-options{
-   margin-bottom: 20px;
- }
+  a {
+    display: block;
+    color: #0062cc;
+  }
+  .categories,
+  .further-options {
+    margin-bottom: 20px;
+  }
 }
 </style>
