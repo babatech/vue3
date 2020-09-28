1d137c1047f425b7fb55c45920e4d92266044a33
Shahab Shakoor <shahabi1209@gmail.com>
2020-09-28 21:51:38 +0200

    bootstrap added with logo

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <nav class="navbar sticky-top navbar-light bg-blue">
+    <a class="navbar-brand" href="#">
+      <img
+        src="./assets/myra-logo.png"
+        height="30"
+        alt="logo"
+      />
+    </a>
+  </nav>
+  <router-view />
 </template>
 
 <style lang="scss">
@@ -14,17 +19,9 @@
   text-align: center;
   color: #2c3e50;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+nav{
+  &.bg-blue {
+    background-color: #082434;
   }
 }
 </style>
