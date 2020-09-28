bb3639a3192ed8bc456d4bd8be9d53408e249c57
Shahab Shakoor <shahabi1209@gmail.com>
2020-09-28 22:35:07 +0200

    Category UI Integrated

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="container home">
     <div class="row">
-      <div class="col-4">
-
+      <div class="col-4 Options">
+        <div class="categories">
+        <b>Filter By:</b>
+        <a v-on:click="filterBy(category)"  v-for="category in categories" v-bind:key="category">{{category}}</a>
+        </div>
+        <div class="further-options">
+        <b>Subscribe:</b>
+        <a v-on:click="subscribeRSS()">RSS</a>
+        <a v-on:click="subscribeEmail()">Email</a>
+        </div>
       </div>
       <div class="col-8">
         <h1>Status message title</h1>
@@ -24,8 +32,34 @@ export default {
   },
   data(){
     return {
-      messages: [1,2,3]
+      messages: [1,2,3],
+      categories: [1,2]
+    }
+  },
+  mounted () {},
+  methods: {
+    filterBy(category) {
+      console.log(category)
+    },
+    subscribeRSS() {
+
+    },
+    subscribeEmail() {
+      
     }
   }
 }
 </script>
+
+<style lang="scss">
+.Options{
+  text-align: left;
+ a{
+   display: block;
+ }
+ .categories,
+ .further-options{
+   margin-bottom: 20px;
+ }
+}
+</style>
