b40bdde04e404499be4d7b795c666939dc2cbdb8
Shahab Shakoor <shahabi1209@gmail.com>
2020-10-04 00:17:23 +0200

    message component as Bootstrap Card

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="message">
-        <div class="body text-left">
+  <div class="card message">
+        <div class="card-body body text-left">
             <div>
                 <b class="">Case ID: {{message.id}}</b>
                 <span class="float-right">{{message.publishDate}}</span>
             </div>
-            <div>
+            <div class="card-title">
                 <h5 class="font-weight-bold">{{message.title}}</h5>
             </div>
             <div>
                 <b>Start Date: </b>
                 {{message.startDate}}
             </div>
-            <div>
+            <div class="card-text">
                 <p class="description">{{message.description}}</p>
             </div>
         </div>
